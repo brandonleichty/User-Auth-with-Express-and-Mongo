32a9d4d397afaa949ad60c9bc0af277d9be3d9ca
Brandon Leichty <brandonl@me.com>
2018-05-08 14:26:52 -0500

    added a GET and POST route for /register. Also changed all functions to arrow functions.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+
+// GET /register
+router.get('/register', (req, res, next) => {
+  return res.send('Register today!')
+});
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  return res.send('User created!')
+});
 
 // GET /
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   return res.render('index', { title: 'Home' });
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
+router.get('/about', (req, res, next) => {
   return res.render('about', { title: 'About' });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
+router.get('/contact', (req, res, next) => {
   return res.render('contact', { title: 'Contact' });
 });
 
